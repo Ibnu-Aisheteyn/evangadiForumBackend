05ec245d26a6bfbefce5b0b0b19ad1bb4eb79382
Kasim Nurlgn <kasimnurlgn9@gmail.com>
2024-11-18 23:32:33 +0300

    some changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 5500; // Use the PORT from Render, fallback to 5500 for local
+const port = process.env.PORT || 5500; // Use the PORT from Render, fallback to 5500 for local
 const dbConnection = require("./db/dbConfig");
 const cors = require("cors");
 
