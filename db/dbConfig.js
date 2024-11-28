ccf73e554f1738e99d83ec5eefc427df4b3193e4
Kasim Nurlgn <kasimnurlgn9@gmail.com>
2024-11-28 08:46:29 +0300

    Some fixes

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,12 +1,14 @@
 const mysql2 = require("mysql2");
+
+// Create a connection pool for efficient handling of multiple connections
 const dbConnection = mysql2.createPool({
   user: process.env.USER,
   database: process.env.DATABASE,
   host: process.env.HOST,
   password: process.env.PASSWORD,
-  connectionLimit: process.env.CONNECTION_LIMIT,
+  port: process.env.PORT || 3306, // Ensure the port is used if provided in the .env file
+  // dbUrl: process.env.DB_URL,
+  // connectionLimit: process.env.CONNECTION_LIMIT || 10, // Optional: Default connection limit if not specified
 });
-
-// console.log(process.env.DATABASE)
 
 module.exports = dbConnection.promise();
